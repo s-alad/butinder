22798ced546cb0a699738bbb476ca1f51a462b17
s-alad <n7.saad@gmail.com>
2024-04-19 14:49:44 -0400

    oops

--- client/firebase/config.ts
+++ client/firebase/config.ts
@@ -8,17 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBGOVBCsD-tov8CK9g15mfooQgmd-DDOnI",
-
-    authDomain: "butinder.firebaseapp.com",
-  
-    projectId: "butinder",
-  
-    storageBucket: "butinder.appspot.com",
-  
-    messagingSenderId: "393645639641",
-  
-    appId: "1:393645639641:web:83c4008e18fe9ed1ebfbc8"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID  
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
