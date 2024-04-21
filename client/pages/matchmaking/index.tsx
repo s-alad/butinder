c0c854fad388ed4a1930f3c56eabc50d12d29a99
Akemi Sai <126123381+achemai@users.noreply.github.com>
2024-04-20 21:52:01 -0400

    added a button on matchmaking page that rediret to profile

--- client/pages/matchmaking/index.tsx
+++ client/pages/matchmaking/index.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
+
 export default function Matchmaking() {
     return (
         <div>
             <h1>Matchmaking</h1>
+            <Link href="/profile">
+            <button type="button">Edit Profile</button>
+            </Link>
         </div>
+        
     )
 }
---
+++ client/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './profile.module.scss';
+
+const ProfilePage = () => {
+  return (
+    <div className={styles.profile}>
+      <h1>Profile</h1>
+      {/* Profile content*/}
+    </div>
+  );
+};
+
+export default ProfilePage;
